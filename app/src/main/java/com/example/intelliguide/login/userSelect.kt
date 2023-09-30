292514e5c1f7c91fcdb8eb73e77380c79867b836
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-09-30 23:16:45 +0530

    Login Ui complete

---
+++ app/src/main/java/com/example/intelliguide/login/userSelect.kt
@@ -0,0 +1,12 @@
+package com.example.intelliguide.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intelliguide.R
+
+class userSelect : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_select)
+    }
+}
