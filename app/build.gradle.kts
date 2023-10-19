8096e0f3bf962fa38280f839bfa80738dbc1c374
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-10-19 23:54:01 +0530

    google dependancy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.intelliguide"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.intelliguide"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/intelliguide/models/TouristModel.kt
@@ -0,0 +1,8 @@
+package com.example.intelliguide.models
+
+data class TouristModel(
+    var age: String? = null,
+    var country: String? = null,
+    var contact: String? = null,
+    var passportNumber: String? = null
+)
