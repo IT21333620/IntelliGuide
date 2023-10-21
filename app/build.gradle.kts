5b68ec5e731cc85ea4e911481a64cbb2d84c3e69
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-10-21 09:22:58 +0530

    error fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -72,4 +73,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
