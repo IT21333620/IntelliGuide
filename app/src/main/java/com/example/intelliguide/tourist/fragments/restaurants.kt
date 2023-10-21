fa75d263b3f0153c7af7d01a5138cf2917851a74
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-10-21 17:25:54 +0530

    tourist fragments

---
+++ app/src/main/java/com/example/intelliguide/tourist/fragments/restaurants.kt
@@ -0,0 +1,21 @@
+package com.example.intelliguide.tourist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.intelliguide.R
+
+class restaurants : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_restaurants, container, false)
+        //enter code here
+        return view
+    }
+
+}
