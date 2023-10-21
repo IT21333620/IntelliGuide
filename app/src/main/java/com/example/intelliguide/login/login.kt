c9101327f655a5cf2cb8e150f4f41817ecc79e88
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-10-21 17:53:03 +0530

    auto login

--- app/src/main/java/com/example/intelliguide/login/login.kt
+++ app/src/main/java/com/example/intelliguide/login/login.kt
@@ -29,6 +29,59 @@ private lateinit var dbRef: DatabaseReference
 
 class login : AppCompatActivity() {
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val userId = currentUser.uid
+
+            val query = FirebaseDatabase.getInstance().getReference("userModel")
+                .orderByChild("userId")
+                .equalTo(userId)
+
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        // Assuming there's only one matching record
+                        for (childSnapshot in snapshot.children) {
+                            val userType = childSnapshot.child("type").getValue(String::class.java)
+                            // Check the user's type and navigate accordingly
+                            when (userType) {
+                                "Tourist" -> {
+                                    val intent = Intent(this@login, tourist_home::class.java)
+                                    startActivity(intent)
+                                    finish() // Finish the current activity to prevent going back to login
+                                    Toast.makeText(baseContext, "Logged in as Tourist.", Toast.LENGTH_SHORT).show()
+                                }
+                                "Hotel Owner" -> {
+                                    val intent = Intent(this@login, startup::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                    Toast.makeText(baseContext, "Logged in as Hotel Owner.", Toast.LENGTH_SHORT).show()
+                                }
+                                "Tourist Police" -> {
+                                    val intent = Intent(this@login, startup::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                    Toast.makeText(baseContext, "Logged in as Tourist Police.", Toast.LENGTH_SHORT).show()
+                                }
+                                else -> {
+                                    Toast.makeText(baseContext, "User Not Found", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle an error here if needed
+                }
+            })
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
