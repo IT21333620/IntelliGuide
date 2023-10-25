d1c2b59336127b45662aeb7d16b5456043953530
IT21365614 <it21365614@my.sliit.lk>
2023-10-25 21:21:00 +0530

    Completion of your posts component and review component

---
+++ app/src/main/java/com/example/intelliguide/models/Review.kt
@@ -0,0 +1,8 @@
+package com.example.intelliguide.models
+
+data class Review(
+    var id: String? = null,
+    var rating: String? = null,
+    var placeName: String? = null,
+    var review: String? = null
+)
